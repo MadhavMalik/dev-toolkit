8ecc0f59156dbab5db25daa86cd1bd6a4761b4ac
madhavmalik <madhavmalik25@gmail.com>
2025-10-14 20:56:41 -0400

    Introduced lambda functions for Crow routes

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 #include "crow.h"
 
 
@@ -7,27 +8,31 @@ public:
     Server();
     ~Server();
     crow::SimpleApp app;
+    // std::function<std::vector<std::string>>()> lambda = []() { std::cout << 42 << std::endl; return std::vector<std::string>{"42"};};
+    std::function<std::vector<std::string>()> lambda = []() {
+        std::cout << 42 << std::endl;
+        return std::vector<std::string>{"42"};
+    };
+    // std::tuple<decltype(lambda)> views = std::make_tuple(lambda);
+    std::vector<decltype(lambda)> views = {lambda};
     void start();
     std::function<std::string(std::string)> view = [](std::string param) {
         std::cout << "Default view run";
         return param;
     };
-    for (filename in os.listdir) {
-        std::function<T> filename = some_lambda_function
-    } 
 };
 
 Server::Server() {};
 Server::~Server() {};
 
 void Server::start() {
-    crow::mustache::set_global_base("/Users/madhavmalik/VSCProjects/dev-toolkit/templates");
+    crow::mustache::set_global_base("/Users/madhavmalik/VSCProjects/dev-toolkit/templates"); //turn this into a relative path
 
     // make this flexible to take in query params
-    CROW_ROUTE(app, "/<string>")([this](std::string parameter){
+    CROW_ROUTE(app, "/<string>/int")([this](std::string parameter, int i){
         auto page = crow::mustache::load("index.html");
         auto param = view(parameter);
-        return page.render({{"parameter",  param}});
+        return page.render({{"parameter",  param}, {"i", i}});
     });
     app.port(18080).multithreaded().run();
 }
@@ -42,6 +47,19 @@ int main() {
         std::cout << "Overloaded view run";
         return param;
     };
+    auto new_func = [] () {std::cout << 43 << std::endl; return std::vector<std::string>{"43"};};
+    // s.views = std::tuple_cat(s.views, std::make_tuple(new_func));
+    s.views.push_back(new_func);
+    // for (auto func : s.views) {
+    //     func();
+    // }
+
+    s.views[0]();
+    s.views[1]();
+
+
+    // std::get<0>(s.views)();
+    // std::get<1>(new_tuple)();
 
     s.start();
 
